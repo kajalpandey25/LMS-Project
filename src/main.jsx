e776eda48756324f5886906edb3bf70e9d47480c
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-09-21 17:11:21 +0530

    redux setup with toolkit done

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <BrowserRouter>
    <App />
    <Toaster />
-  </BrowserRouter>  
+   </BrowserRouter> 
+   </Provider>
 )
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './slices/authSlice';
+
+
+const store = configureStore({
+    reducer :{
+        auth: authReducer
+    },
+    devTools: true
+
+});
+
+export default store;
